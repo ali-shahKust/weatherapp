40bf4d1f22f45be5307f65d45b3c743e4e2c31f3
ali-shahKust <syed.ali.shah3938@gmail.com>
2020-05-04 13:12:47 +0500

    Changes in classes

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'package:clima/res.dart';
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
+import 'package:android_intent/android_intent.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -7,16 +10,52 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocation();
+    //openLocationSetting();
+
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+    return MaterialApp(
+      home: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(Res.location_background), fit: BoxFit.fill)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Container(
+            child: Center(
+              child: RaisedButton(
+                color: Color(0xff000000),
+                  shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(18.0),
+                  side: BorderSide(color: Colors.white70)
+              ),
+                onPressed: () {
+                  //Get the current location
+                  openLocationSetting();
+                },
+                child: Text('Get Location',style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),),
+              ),
+            )
+          ),
         ),
       ),
     );
+  }
+void getLocation() async{
+  Location location = new Location();
+  await location.getCurrentLocation();
+  }
+  void openLocationSetting() async {
+    final AndroidIntent intent = new AndroidIntent(
+      action: 'android.settings.LOCATION_SOURCE_SETTINGS',
+    );
+    await intent.launch();
   }
 }
